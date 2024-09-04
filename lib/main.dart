92ed57c116b3f041e2b90804201d12154d4047a8
mjad <mjad3780@gmail.com>
2024-09-04 20:27:01 +0300

    test one signal

--- lib/main.dart
+++ lib/main.dart
@@ -21,9 +21,7 @@ void main() async {
 
   OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
   OneSignal.initialize("9c37a804-6bb8-4055-96f4-a56308ae8b63");
-
   OneSignal.Notifications.requestPermission(true);
-  OneSignalPushSubscription();
 
   // ]);
   setupServise();
@@ -33,38 +31,4 @@ void main() async {
   runApp(const name());
 }
 
-class name extends StatefulWidget {
-  const name({super.key});
 
-  @override
-  State<name> createState() => _nameState();
-}
-
-class _nameState extends State<name> {
-  hellp() {
-    OneSignal.User.getOnesignalId();
-    OneSignal.Notifications;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: GestureDetector(
-              onTap: () async {
-                SendPushNotifications.getAllNotifications();
-                //69679502-8c9a-49ed-a18a-96c5447b1a94
-                await SendPushNotifications.checkNotificationStatus(
-                    '7cfee27a-0863-456f-bd95-ec1e7d241876');
-
-                final id = OneSignal.User.pushSubscription.id;
-
-                log(id.toString());
-              },
-              child: const Text('data')),
-        ),
-      ),
-    );
-  }
-}
